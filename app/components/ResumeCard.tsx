2d26ab5ac3a95e868b6476e4a88f71728ded7407
B_M_Yashwanth <bhagyapushpagiri17@gmail.com>
2025-08-10 17:44:10 +0530

    fetch real resumes into home page

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,28 +1,50 @@
-import React from 'react'
-import { Link } from 'react-router'
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router'
 import ScoreCircle from './ScoreCircle'
+import { usePuterStore } from '~/lib/puter';
+import { resumes } from 'constnants';
 
 const ResumeCard = ( {resume: {id,companyName,jobTitle,feedback,imagePath}}:{resume:Resume}) => {
-  return (
+    const navigate = useNavigate();
+    const [resumeUrl, setResumeUrl] = useState('')
+    const {fs} = usePuterStore();
+    
+    
+      useEffect(
+        ()=>{
+          const loadResume=async()=>{
+            const blob = await fs.read(imagePath)
+            if(!blob)return 
+    
+            let url = URL.createObjectURL(blob);
+            setResumeUrl(url);
+          } 
+          loadResume();
+        },[imagePath])
+  
+    return (
     <Link to ={`/resume/${id}`} className='resume-card animate-in fade-in duration-1000'>
         <div className="resume-card-header">
             <div className="flex flex-col gap-2">
-                <h2 className='text-black font-bold break-words'>{companyName}</h2>
-                <h3 className='text-lg break-words text-gray0599'>{jobTitle}</h3>
+                {companyName && <h2 className='text-black font-bold break-words'>{companyName}</h2>}
+                {jobTitle && <h3 className='text-lg break-words text-gray0599'>{jobTitle}</h3>}
+                {!jobTitle && !companyName && (
+                    <h2 className="!text-black font-bold break-words">Resume</h2>
+                )}
             </div>
         <div className="flex-shrink-0">
                 <ScoreCircle score={feedback.overallScore}/>
             </div>
         </div>
-        <div className="gradient-border animate-in duration-1000">
+        {resumes && (<div className="gradient-border animate-in duration-1000">
             <div className="w-full h-full">
                 <img
-                    src={imagePath}
+                    src={resumeUrl}
                     alt="resume"
                     className='w-full h-[350px] max-sm:h-[200px] object-cover object-top duration-1000'
                 />
             </div>
-        </div>
+        </div>)}
     </Link>
   )
 }
